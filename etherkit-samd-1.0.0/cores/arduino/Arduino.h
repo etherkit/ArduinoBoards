29210d652d7a5ece0029ef1d72e626ec6e86944c
Jason Milldrum <milldrum@gmail.com>
2017-02-22 14:33:58 -0800

    Add CMSIS-Atmel

--- etherkit-samd-1.0.0/cores/arduino/Arduino.h
+++ etherkit-samd-1.0.0/cores/arduino/Arduino.h
@@ -35,6 +35,7 @@ typedef uint16_t word;
 //
 #include "avr/pgmspace.h"
 #include "avr/interrupt.h"
+#include "avr/io.h"
 
 #include "binary.h"
 #include "itoa.h"
@@ -53,6 +54,10 @@ extern "C"{
 #define microsecondsToClockCycles(a) ( (a) * (SystemCoreClock / 1000000L) )
 
 void yield( void ) ;
+
+/* system functions */
+int main( void );
+void init( void );
 
 /* sketch */
 void setup( void ) ;
